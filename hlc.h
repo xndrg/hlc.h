c7799b0d6cee25eab8f716870e57be14ec3431aa
xndrg <galay196@gmail.com>
2024-10-29 18:21:56 +0300

    String_View

--- hlc.h
+++ hlc.h
@@ -15,30 +15,47 @@ TODO:
 #include <errno.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
-#define HLC_UNUSED(value) (void)(value)
-#define HLC_TODO(msg) do { fprintf(stderr, "%s:%d: TODO %s\n", __FILE__, __LINE__, msg); abort(); } while(0)
-#define HLC_UNREACHABLE(msg) do { fprintf(stderr, "%s:%d UNREACHABLE: %s\n", __FILE__, __LINE__, msg); abort(); } while(0)
+#define UNUSED(value) (void)(value)
+#define TODO(msg) do { fprintf(stderr, "%s:%d: TODO %s\n", __FILE__, __LINE__, msg); abort(); } while(0)
+#define UNREACHABLE(msg) do { fprintf(stderr, "%s:%d UNREACHABLE: %s\n", __FILE__, __LINE__, msg); abort(); } while(0)
 
-#define HLC_ARRAY_LEN(array) (sizeof(array) / sizeof(array[0]))
-#define HLC_ARRAY_GET(array, index) \
-(assert((size_t)index < HLC_ARRAY_LEN(array)), array[(size_t)index])
-
-#define hlc_return_defer(value) do { result = (value); goto defer; } while(0)
+#define ARRAY_LEN(array) (sizeof(array) / sizeof(array[0]))
 
 typedef struct {
     char  *data;
     size_t size;
-} Hlc_File_Buffer;
+} File_Buffer;
 
-Hlc_File_Buffer hlc_read_entire_file(const char *file_path);
-#define hlc_fb_free(fb) free((fb).data)
+File_Buffer read_entire_file(const char *file_path);
+#define fb_free(fb) free((fb).data)
+
+typedef struct {
+    const char *data;
+    size_t size;
+} String_View;
+
+typedef bool(*String_View_Predicate)(char);
+
+#define SV_LITERAL(__literal__) (String_View) { .data = (__literal__), .size = sizeof(__literal__) - 1}
+
+#define SV_Fmt "%.*s"
+#define SV_Arg(__sv__) (int) (__sv__).size, (__sv__).data
+
+String_View sv_new(const char *data, size_t size);
+bool        sv_equals(const String_View a, const String_View b);
+bool        sv_has_prefix(String_View sv, String_View prefix);
+String_View sv_chop(String_View *sv, size_t n);
+String_View sv_chop_prefix(String_View *sv, String_View prefix);
+String_View sv_chop_while(String_View *sv, String_View_Predicate predicate);
+bool sv_is_alpha(const char c);
 
 #ifdef HLC_IMPLEMENTATION
 
-Hlc_File_Buffer hlc_read_entire_file(const char *file_path)
+File_Buffer read_entire_file(const char *file_path)
 {
-    Hlc_File_Buffer fb = {0};
+    File_Buffer fb = {0};
 
     FILE *f = fopen(file_path, "rb");
     if (f == NULL)                 goto defer;
@@ -57,6 +74,71 @@ Hlc_File_Buffer hlc_read_entire_file(const char *file_path)
 defer:
     if (f) fclose(f);
     return fb;
+}
+
+String_View sv_new(const char *data, size_t size)
+{
+    return (String_View) {
+	.data = data,
+	.size = size,
+    };
+}
+
+bool sv_equals(const String_View a, const String_View b)
+{
+    if (a.size != b.size) {
+	return false;
+    }
+
+    return memcmp(a.data, b.data, a.size) == 0;
+}
+
+bool sv_has_prefix(String_View sv, String_View prefix)
+{
+    if (sv.size < prefix.size) {
+	return false;
+    }
+
+    String_View substr = sv_new(sv.data, prefix.size);
+    return sv_equals(substr, prefix);
+}
+
+String_View sv_chop(String_View *sv, size_t n)
+{
+    if (sv->size < n) {
+	sv->size = n;
+    }
+
+    String_View result = sv_new(sv->data, n);
+
+    sv->size = sv->size - n;
+    sv->data = sv->data + n;
+
+    return result;
+}
+
+String_View sv_chop_prefix(String_View *sv, String_View prefix)
+{
+    if (!sv_has_prefix(*sv, prefix)) {
+	return sv_new(NULL, 0);
+    }
+
+    return sv_chop(sv, prefix.size);
+}
+
+String_View sv_chop_while(String_View *sv, String_View_Predicate predicate)
+{
+    size_t i = 0;
+    
+    while (i < sv->size && predicate(sv->data[i])) {
+	i += 1;
+    }
+
+    return sv_chop(sv, i);
+}
+
+bool sv_is_alpha(const char c) {
+    return (bool) isalpha((int) c);
 }
 
 #endif // HLC_IMPLEMENTATION
